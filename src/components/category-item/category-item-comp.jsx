d40db5c7a804a98e2dbd3ccbe0d12edc09475dcb
Salah Eddine <salaheddine250@hotmail.com>
2022-06-12 22:06:42 +0100

    first commit, making a homepage with directory and directory items component

---
+++ src/components/category-item/category-item-comp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './category-item-styles.scss'
+
+const Category = ({category}) => {
+  const {title, imageUrl, size} = category;
+
+  return (
+    <div  className={`category-item ${size}`}>
+      <div 
+        className='background-image' 
+        style = {{backgroundImage : `url(${imageUrl})`
+        }} 
+      />
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>SHOP NOW</p>
+      </div>
+          
+    </div>
+  )
+}
+
+export default Category
---
+++ src/pages/homepage/homepage-comp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Directory from '../../components/directory/directory-comp'
+
+const Homeage = () => {
+  
+  return (
+    <Directory/>
+  )
+}
+
+export default Homeage
