f1a22109d935c11ccb30bec2ab2e302d32e6afa9
Salah Eddine <salaheddine250@hotmail.com>
2022-09-15 17:53:09 +0100

    updated the app to a PWA

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { Elements } from "@stripe/react-stripe-js";
 
 import { store, persistor } from "./store/store";
 import { stripePromise } from "./stripe/stripe";
+import * as registerServiceWorker from "./serviceWorkerRegistration"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -27,6 +28,7 @@ root.render(
   </React.StrictMode>
 );
 
+registerServiceWorker.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
